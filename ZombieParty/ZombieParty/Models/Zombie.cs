177bc26e7d1b6bbdbc25d0c1e27fbdbae0fea91c
Issam <2050745@cegepmontpetit.ca>
2023-06-23 22:11:04 -0400

    FCT class Zombie
    Ajout de la classe Zombie

---
+++ ZombieParty/ZombieParty/Models/Zombie.cs
@@ -0,0 +1,10 @@
+﻿namespace ZombieParty.Models
+{
+    public class Zombie
+    {
+            string Name { get; set; }
+            string Type { get; set; }
+            int Point { get; set; }
+
+    }
+}
